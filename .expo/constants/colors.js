0913e96ebf51acd83ea47dfb8b12f8672d26d20c
다연집컴 <jewelry0706@naver.com>
2024-11-05 02:28:02 +0900

    피그마 기반으로 필요해보이는 파일들 추가

---
+++ .expo/constants/colors.js
@@ -0,0 +1,25 @@
+// 컬러팔레트 색상들 추가해둠
+
+const Colors = {
+  lightBeige: "#FEFAFB",
+  primaryBeige: "#FCF1EC",
+  pink10: "#F4C8D7",
+  pink20: "#F09AB4",
+  pink30: "#EC6C90",
+  pink40: "#EA466E",
+  red10: "#E4324F",
+  red20: "#B5233F",
+  darkRed10: "#86192F",
+  darkRed20: "#570D1E",
+  primaryGreen: "#B4DF7E",
+  white: "#FFFFFF",
+  gray10: "#F8F8F8",
+  gray15: "#F2F2F2",
+  gray20: "#D1D1D1",
+  gray30: "#A8A8A8",
+  gray40: "#656565",
+  gray45: "#4E4E4E",
+  gray50: "#1F1F1F",
+};
+
+export default Colors;
---
+++ components/feed/FeedItem.js
@@ -0,0 +1,2 @@
+//피드의 전체 구조를 담당하는 컴포넌트입니다.
+//AccountInfo, PostContent, ReactionButtons 컴포넌트를 포함하여 피드의 하나의 항목을 완성합니다.
