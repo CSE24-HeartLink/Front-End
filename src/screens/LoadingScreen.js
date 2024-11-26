b892e4eab6924f701bee50ea693a83af28fef8d0
Dayeon <jewelry0706@naver.com>
2024-11-26 12:40:45 +0900

    fix: 로딩스크린 수정

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,28 +1,24 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  ActivityIndicator,
-  StyleSheet,
-  Text,
-  Animated,
-} from "react-native";
+import { View, StyleSheet, Animated } from "react-native";
+import LottieView from "lottie-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Colors from "../constants/colors";
 
-const LoadingScreen = ({ message = "로딩중입니다>__<" }) => {
-  const [fadeAnim] = useState(new Animated.Value(0.4));
+const LoadingScreen = () => {
+  const [scaleAnim] = useState(new Animated.Value(1)); // 텍스트 크기 애니메이션
 
   useEffect(() => {
+    // 텍스트 크기 애니메이션 반복
     Animated.loop(
       Animated.sequence([
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 1000,
+        Animated.timing(scaleAnim, {
+          toValue: 1.2, // 커짐
+          duration: 500,
           useNativeDriver: true,
         }),
-        Animated.timing(fadeAnim, {
-          toValue: 0.4,
-          duration: 1000,
+        Animated.timing(scaleAnim, {
+          toValue: 1, // 작아짐
+          duration: 500,
           useNativeDriver: true,
         }),
       ])
@@ -32,10 +28,24 @@ const LoadingScreen = ({ message = "로딩중입니다>__<" }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <ActivityIndicator size="large" color={Colors.red20} />
-        <Animated.Text style={[styles.loadingText, { opacity: fadeAnim }]}>
-          {message}
-        </Animated.Text>
+        {/* 애니메이션과 텍스트를 감싸는 뷰 */}
+        <View style={styles.animationContainer}>
+          <LottieView
+            source={require("../../../Front-End/assets/animations/heartbeat.json")}
+            autoPlay
+            loop
+            style={styles.lottie}
+          />
+
+          <Animated.Text
+            style={[
+              styles.loadingText,
+              { transform: [{ scale: scaleAnim }] }, // 크기 애니메이션
+            ]}
+          >
+            마음을 연결중이에요.{"\n"}잠시만 기다려주세요!
+          </Animated.Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -52,14 +62,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primaryBeige,
   },
+  animationContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 80,
+  },
+  lottie: {
+    width: 200,
+    height: 200,
+  },
   loadingText: {
     marginTop: 16,
     fontSize: 16,
     letterSpacing: -0.3,
-    color: Colors.gray80,
+    color: Colors.pink40,
     fontFamily: "Pretendard",
     fontWeight: "600",
     includeFontPadding: false,
+    textAlign: "center",
+    lineHeight: 22,
   },
 });
 
